77bdef3b7299c7617aaa854fe2dd142173623684
Brenda Noelia Veramendi <brendaveramendi@gmail.com>
2023-09-15 11:25:32 -0300

    conecion a base de datos

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,8 @@
 import { DataSource } from "typeorm";
 import { User } from "./entities/User";
+require('dotenv').config();
 
-import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from "./config";
+// import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from "./config";
 import { Rol } from "./entities/Rol";
 import { Orden } from "./entities/Orden";
 import { Afiliado } from "./entities/Afiliado";
@@ -11,11 +12,11 @@ import { Cuota } from "./entities/Cuota";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: DB_HOST,
+  host: process.env.DB_HOST,
   port: 3306,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   // logging: true, // muestra peticiones a la bd
   synchronize: true,
   entities: [User, Rol, Orden, Afiliado, Domicilio, Comercio, Cuota ],
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import "reflect-metadata";
 import app from './app';
 import { AppDataSource } from "./db";
-import {PORT} from './config'
-
+// import {PORT} from './config'
+require('dotenv').config();
 
 async function main() {
   try {
     await AppDataSource.initialize();
     console.log('Database Connected...');
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
+    app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`))
   } catch (error) {
     console.error(error);
   }
